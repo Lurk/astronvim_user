0813953070ec576fc94d42b9c8182694f591b0f1
Serhiy Barhamon <Bargamon@gmail.com>
2023-07-30 00:46:42 +0200

    use community plugins

--- plugins.lua
+++ plugins.lua
@@ -1,54 +1,30 @@
 return {
   { "sainnhe/gruvbox-material" },
+  { "AstroNvim/astrocommunity" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.color.twilight-nvim", enabled = true },
+  { import = "astrocommunity.editing-support.auto-save-nvim" },
   {
-    "github/copilot.vim",
-    event = "VeryLazy",
-    config = function()
-      -- copilot assume mapped
-      vim.g.copilot_assume_mapped = true
-      vim.g.copilot_no_tab_map = true
-    end,
+    import = "astrocommunity.motion.mini-move",
   },
   {
-    "zbirenbaum/copilot-cmp",
-    opts = {},
-    config = function(_, opts)
-      local copilot_cmp = require("copilot_cmp")
-      copilot_cmp.setup(opts)
-      -- attach cmp source whenever copilot attaches
-      -- fixes lazy-loading issues with the copilot cmp source
-
-      copilot_cmp._on_insert_enter()
-    end,
-  },
-  {     -- override nvim-cmp plugin
-    "hrsh7th/nvim-cmp",
-    -- override the options table that is used in the `require("cmp").setup()` call
-    opts = function(_, opts)
-      -- opts parameter is the default options table
-      -- the function is lazy loaded so cmp is able to be required
-      local cmp = require "cmp"
-      -- modify the sources part of the options table
-      opts.sources = cmp.config.sources {
-        { name = "copilot",  priority = 1001 },
-        { name = "nvim_lsp", priority = 1000 },
-        { name = "luasnip",  priority = 750 },
-        { name = "buffer",   priority = 500 },
-        { name = "path",     priority = 250 },
-      }
-
-      opts.mapping = {
-        ['<C-g>'] = cmp.mapping(function(fallback)
-          vim.api.nvim_feedkeys(vim.fn['copilot#Accept'](vim.api.nvim_replace_termcodes('<Tab>', true, true, true)), 'n',
-            true)
-        end)
-      }
-      opts.experimental = {
-        ghost_text = false -- this feature conflict with copilot.vim's preview.
-      }
-
-      -- return the new table to be used
-      return opts
-    end,
+    "echasnovski/mini.move",
+    keys = {
+      { "<S-Up>", mode = "n", desc = "Move line up" },
+      { "<S-Down>", mode = "n", desc = "Move line Down" },
+      { "<S-Up>", mode = "v", desc = "Move selection up" },
+      { "<S-Down>", mode = "v", desc = "Move selection right" },
+    },
+    opts = {
+      mappings = {
+        down = "<S-Down>",
+        up = "<S-Up>",
+        line_down = "<S-Down>",
+        line_up = "<S-Up>",
+      },
+    },
   },
 }
--- readme.md
+++ readme.md
@@ -8,3 +8,8 @@ after installing [AstroNvim](https://astronvim.com/)
 cd ~/.config/nvim/lua/
 git clone https://github.com/Lurk/astronvim_user.git user
 ```
+
+## mini-move
+
+<S+Up>      - move up line/selection
+<S+Down>    - move down line/selection 
