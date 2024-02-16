732c46b6425f0796ea03ebe889753c16f7b1e808
Serhiy Barhamon <serhiy.barhamon@check24.de>
2024-02-16 15:09:17 +0100

    Fix undo/redo with autosave + format on save

--- lsp.lua
+++ lsp.lua
@@ -1,14 +1,7 @@
 return {
   lsp = {
     formatting = {
-      format_on_save = true, -- enable or disable automatic formatting on save
-      timeout_ms = 3200, -- adjust the timeout_ms variable for formatting
-      filter = function(client)
-        -- if vim.bo.filetype == "css" then return client.name == "null-ls" end
-
-        -- enable all other clients
-        return true
-      end,
+      format_on_save = false, -- enable or disable automatic formatting on save
     },
   },
 }
--- plugins.lua
+++ plugins.lua
@@ -8,6 +8,32 @@ return {
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   {
+    "okuuva/auto-save.nvim",
+    event = { "InsertLeave", "TextChanged" },
+    opts = {
+      noautocmd = true,
+    },
+    init = function()
+      -- autosave + autoformat on save breaks undo/redo flow. I do not want to give up autosave, so here I am disabling 
+      -- autoformat on autosave.
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "AutoSaveWritePre",
+        callback = function()
+          vim.g._AUTOFORMAT_ENABLED = vim.g.autoformat_enabled
+          vim.g.autoformat_enabled = false
+        end,
+      })
+      -- restore autoformat status after autosave
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "AutoSaveWritePost",
+        callback = function()
+          vim.g.autoformat_enabled = vim.g._AUTOFORMAT_ENABLED
+        end,
+      })
+    end,
+  },
+  { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
+  {
     "rcarriga/nvim-notify",
     opts = { stages = "static" },
   },
